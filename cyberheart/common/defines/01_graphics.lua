d26d99fd5f99a16ef010455983f1cc9426a77679
Albanianandproud <Bigdickautomobiles@gmail.com>
2023-07-12 21:58:30 +0100

    Western Seaboard VPs
    - Added VPs to numerous states on Western Seaboard
    - Changed Defines so VPs fade at a much longer distance (Capital VP a little buggy, will need to look into)
    - Added MP to a few VPs still needs to be glossed over

---
+++ cyberheart/common/defines/01_graphics.lua
@@ -0,0 +1,8 @@
+---Graphical Data---
+-- Defines should typically follow this style... NDefines.N{Parenthesis Bracked}.DEFINE
+
+	NDefines.NGraphics.VICTORY_POINT_LEVELS = 2
+	NDefines.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 10} -- After this amount of VP the map icon becomes bigger dot.
+	NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {500, 750, 1250} -- At what camera distance the VP name text disappears.
+	NDefines.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {750, 1250, 1750} -- At what distance VPs are hidden
+	NDefines.NGraphics.CAPITAL_ICON_CUTOFF = 2200
