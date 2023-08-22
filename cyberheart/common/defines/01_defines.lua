d4ea2914a920d4837580cece959ffea83e662e30
Albanianandproud <Bigdickautomobiles@gmail.com>
2023-08-22 17:16:35 +0100

    Update
    - Begin progress on Vehicle tech tree
    - Update Ministers tab
    - Buffed starting state_category building slots
    - added starting industry for Free State Nations

--- cyberheart/common/defines/01_defines.lua
+++ cyberheart/common/defines/01_defines.lua
@@ -1,3 +1,8 @@
 ---Game Date---
 NDefines.NGame.START_DATE = "2077.1.1.12"
 NDefines.NGame.END_DATE = "2100.1.1.1"
+
+---Buildings---
+NDefines.NBuildings.MAX_SHARED_SLOTS = 40
+NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS = 0.0
+NDefines.NBuildings.SUPPLY_ROUTE_RESOURCE_BONUS = 0.0
