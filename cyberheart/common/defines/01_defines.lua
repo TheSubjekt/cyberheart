f1c90f2f647c685b299326894f396c055db0ba3f
Albanianandproud <Bigdickautomobiles@gmail.com>
2023-08-26 17:47:38 +0100

    Trade UI finished

--- cyberheart/common/defines/01_defines.lua
+++ cyberheart/common/defines/01_defines.lua
@@ -3,6 +3,6 @@ NDefines.NGame.START_DATE = "2077.1.1.12"
 NDefines.NGame.END_DATE = "2100.1.1.1"
 
 ---Buildings---
-NDefines.NBuildings.MAX_SHARED_SLOTS = 40
+NDefines.NBuildings.MAX_SHARED_SLOTS = 50
 NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS = 0.0
 NDefines.NBuildings.SUPPLY_ROUTE_RESOURCE_BONUS = 0.0
